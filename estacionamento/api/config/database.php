fb794963d3a9828f40917143f3e7c757913a7de5
Ricardo Souza <gasparsrick@gmail.com>
2025-05-22 08:02:06 -0300

    Alteração do banco de dados estacionamentos

--- estacionamento/api/config/database.php
+++ estacionamento/api/config/database.php
@@ -6,11 +6,11 @@
  */
 
 return [
-    'host' => 'localhost',
-    'port' => '5432',
-    'database' => 'estacionamento_db',
-    'username' => 'postgres',
-    'password' => 'postgres',
-    'charset' => 'utf8',
-    'schema' => 'public'
-];
+       'host' => '158.220.108.253',
+       'port' => '5432',
+       'database' => 'estacionamentos',
+       'username' => 'postgres',
+       'password' => '671afde520b8c54c42843578a31d05d8',
+       'charset' => 'utf8',
+       'schema' => 'public'
+   ];
